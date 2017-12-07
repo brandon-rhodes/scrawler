9f012a27b4b10009c1f6cd6ff319d691f18ae70e
Brandon Rhodes <brandon@rhodesmill.org>
2017-12-07 10:24:05 -0500

    Move zen message up a bit

--- zen.py
+++ zen.py
@@ -3,9 +3,9 @@ import scrawler.api as sc
 def simple_animation(term):
     y = term.center_y
     duration = term.Hz
-    line1 = sc.center_text(term, y-1, 'Beautiful')
-    line2 = sc.center_text(term, y, 'is better')
-    line3 = sc.center_text(term, y+1, 'than ugly')
+    line1 = sc.center_text(term, y-2, 'Beautiful')
+    line2 = sc.center_text(term, y-1, 'is better')
+    line3 = sc.center_text(term, y, 'than ugly')
     yield from sc.fade_in(line1, duration)
     yield from sc.simulcast(line1, sc.fade_in(line2, duration))
     yield from sc.simulcast(line1, line2, sc.fade_in(line3, duration))

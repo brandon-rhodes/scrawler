96f69793232287b1c07bdde6c169a02eacf79394
Brandon Rhodes <brandon@rhodesmill.org>
2017-12-07 10:24:13 -0500

    Remove obsolete blend() routine

--- scrawler/colorlib.py
+++ scrawler/colorlib.py
@@ -16,10 +16,3 @@ def apply_alpha(color_plus_alpha, color):
 #     return (int(color[0], 16) / 15.0,
 #             int(color[1], 16) / 15.0,
 #             int(color[2], 16) / 15.0)
-
-def blend(fraction, color1, color2):
-    r1, g1, b1 = to_rgb(color1)
-    r2, g2, b2 = to_rgb(color2)
-    return (r1 + (r2 - r1) * fraction,
-            g1 + (g2 - g1) * fraction,
-            b1 + (b2 - b1) * fraction)
